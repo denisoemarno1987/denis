aebf7acca077c41a7863246618290edafc465850
Bang Den <denisoemarno@aol.com>
2017-06-08 15:02:24 +0700

    Add files via upload

---
+++ front/layoutFront/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Conference - Responsive HTML5 Template</title>
+
+    <!-- favicon -->
+    <link href="favicon.png" rel=icon>
+
+    <!-- web-fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,500' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Style CSS -->
+    <link href="../bootstrap/css/style.css" rel="stylesheet">
+
+    <!-- jquery -->
+    <script src="../bootstrap/js/jquery-1.9.1.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+
+    
+</head>
